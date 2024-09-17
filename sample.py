cb568d1659bc4d6bd28ee55c8bc96172f022f92b
krunalkumar0354 <krunalkumar@zeni.ai>
2024-09-17 10:57:24 +0530

    sample python code to use fine tuned model is added.

---
+++ sample.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the OpenAI API key from the environment variables
+api_key = os.getenv('APIKEY')
+
+client = OpenAI(api_key=api_key)
+if not api_key:
+    raise ValueError("API key not found in environment variables.")
+
+client = OpenAI(api_key=api_key)
+
+completion = client.chat.completions.create(
+  model="ft:gpt-4o-mini-2024-07-18:zeni-revops::A8KqPcyb",
+  messages=[
+        {"role": "user", "content": "What's the weather like today?"}
+    ]
+)
+
+print(completion.choices[0].message.content)
