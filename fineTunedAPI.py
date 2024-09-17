8ec15cb4247963e162b7018ead9ee755b5a72fd9
krunalkumar0354 <krunalkumar@zeni.ai>
2024-09-18 01:59:01 +0530

    test data added.

--- fineTunedAPI.py
+++ fineTunedAPI.py
@@ -2,35 +2,26 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Get the OpenAI API key from the environment variables
-api_key = os.getenv('APIKEY')
+api_key = os.getenv('APIKEYP')
 
 client = OpenAI(api_key=api_key)
 
-# Check if the API key is loaded correctly
 if not api_key:
     raise ValueError("API key not found in environment variables.")
 
-# Set the OpenAI API key
-
 try:
-    # List available models
     response = client.models.list()
-
-    # Print model IDs and names
-    # for model in response.data:
-    #     print(f"Model ID: {model['id']}, Name: {model.get('name', 'No Name')}")
-
-    # Make a request to the fine-tuned model
-    response = client.chat.completions.create(model="ft:gpt-4o-mini-2024-07-18:zeni-revops:myfinetunedchatwithvalidation:A87Wx5pd",  # Replace with your fine-tuned model ID
+    #ft:gpt-4o-mini-2024-07-18:zeni-revops:churn-reason-dictionary-2:A8KT8btt
+    #ft:gpt-4o-mini-2024-07-18:zeni-revops:myfinetunedchatwithvalidation:A87Wx5pd
+    print(response)
+    response = client.chat.completions.create(model="ft:gpt-4o-mini-2024-07-18:zeni-revops:churn-reason-dictionary-2:A8KT8btt",
+                                              
     messages=[
-        {"role": "user", "content": "What's the weather like today?"}
+        {"role": "user", "content": "FitSwoop Inc. churned due to a transition to an in-house team, indicating dissatisfaction with the support received, especially from their controller (Ticket Notes). They struggled with dashboard navigation and lacked effective help for reporting (Company Notes)."}
     ])
 
-    # Print the assistant's response
     print(response.choices[0].message.content)
 
 except Exception as e:
